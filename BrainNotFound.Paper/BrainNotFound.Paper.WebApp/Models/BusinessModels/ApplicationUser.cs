d28126b4c3ba58b774ba6269bec09e192e687f5f
Matthew Snyder <124906@studentnet.int>
2019-02-22 19:37:43 -0600

    Various changes

--- BrainNotFound.Paper/BrainNotFound.Paper.WebApp/Models/BusinessModels/ApplicationUser.cs
+++ BrainNotFound.Paper/BrainNotFound.Paper.WebApp/Models/BusinessModels/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace BrainNotFound.Paper.WebApp.Models.BusinessModels
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Salutation { get; set; }
