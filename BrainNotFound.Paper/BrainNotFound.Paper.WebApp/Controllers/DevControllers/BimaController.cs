61e7316b731d087202634a98c021e0da85f7453f
Abmael Silva <abmael.silva@me.com>
2019-02-23 19:36:59 -0600

    Did Alot

--- BrainNotFound.Paper/BrainNotFound.Paper.WebApp/Controllers/DevControllers/BimaController.cs
+++ BrainNotFound.Paper/BrainNotFound.Paper.WebApp/Controllers/DevControllers/BimaController.cs
@@ -8,26 +8,66 @@ using Microsoft.AspNetCore.Mvc;
 using BrainNotFound.Paper.WebApp.Models;
 using Microsoft.AspNetCore.Identity;
 using BrainNotFound.Paper.WebApp.Models.BusinessModels;
+using System.IO;
+using CsvHelper;
 
 namespace BrainNotFound.Paper.WebApp.Controllers.DevControllers
 {
     public class BimaController : Controller
     {
-
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
 
         private PaperDbContext _context;
 
-
         public async Task<IActionResult> Run()
         {
-            var NewUserFetched = await _userManager.FindByEmailAsync("abmael.silva@me.com");
-            await _userManager.AddToRoleAsync(NewUserFetched, "Admin");
+            using (var reader = new StreamReader("SampleData/Instructor_Sample_Data.csv"))
+            using (var csv = new CsvReader(reader))
+            {
+                csv.Configuration.HeaderValidated = null;
+                csv.Configuration.MissingFieldFound = null;
+                var instructors = csv.GetRecords<CreateInstructorViewModel>();
+                foreach (CreateInstructorViewModel model in instructors)
+                {
+
+                    var newInstructor = new ApplicationUser()
+                    {
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        Salutation = model.Salutation,
+                        UserName = model.UserName,
+                        Email = model.Email,
+                        PhoneNumber = model.PhoneNumber,
+
+                    };
+
+                    //Create a new Application User
+                    var result = await _userManager.CreateAsync(newInstructor, model.Password);
 
 
-            return RedirectToAction("ForceLogin", "Root");
+                    if (result.Succeeded)
+                    {
+                        //Fetch created user
+                        var CreatedUser = await _userManager.FindByEmailAsync(model.Email);
+
+                        //Add instructor role to created Application User
+                        await _userManager.AddToRoleAsync(CreatedUser, "Instructor");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ViewData["Message"] += error.Description;
+                        }
+                    }
+                }
+            }
+
+            //path 'C:\Users\Abmael Silva\GitHub\CS414_Paper\SampleData\Instructor_Sample_Data.csv'.
+            //     'C:\Users\Abmael Silva\GitHub\CS414_Paper\BrainNotFound.Paper\BrainNotFound.Paper.WebApp\SampleData\Instructor_Sample_Data.csv
+            return View("TestView");
         }
 
         public async Task<IActionResult> InitalSetUp()
