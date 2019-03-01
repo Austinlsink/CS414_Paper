dd5565a4d4992c82ae83d21fee96a5d9f87be1b1
Abmael <abmael.silva@me.com>
2019-02-28 21:15:48 -0600

    Signed-off-by: Abmael <abmael.silva@me.com>

---
+++ BrainNotFound.Paper/BrainNotFound.Paper/Services/SalutationTypes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrainNotFound.Paper.Services
+{
+    public static class SalutationType
+    {
+
+        public static string Mr => "Mr";
+        public static string Mrs => "Mrs";
+        public static string Ms => "Ms";
+        public static string Miss => "Miss";
+        public static string Dr => "Dr";
+
+        public static List<string> All
+        {
+            get
+            {
+                return new List<string> { Mr, Mrs, Ms, Miss, Dr }; ;
+            }
+        }
+    }
+}
