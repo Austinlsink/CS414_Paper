a43c5710d7f3337fea58c34a78fc58fc0cff129c
Abmael <abmael.silva@me.com>
2019-03-07 17:32:27 -0600

    Signed-off-by: Abmael <abmael.silva@me.com>

---
+++ BrainNotFound.Paper/BrainNotFound.Paper/api/StudentsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BrainNotFound.Paper.Models.BusinessModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrainNotFound.Paper.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly PaperDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public StudentsController(PaperDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: api/Students
+        [HttpGet]
+        public async Task<IEnumerable<ApplicationUser>> Get()
+        {
+            var allStudents = (await _userManager.GetUsersInRoleAsync("students")).OrderBy(o => o.FirstName).ToList();
+            return allStudents;
+
+
+        }
+
+        // GET: api/Students/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Students
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Students/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
