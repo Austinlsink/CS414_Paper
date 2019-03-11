97b10e0809fe2aa5683995b584f9f146c12a8b12
127321 <nick.goblirsch@students.pcci.edu>
2019-03-11 18:49:31 -0500

    merging

---
+++ BrainNotFound.Paper/BrainNotFound.Paper/wwwroot/js/CreateTest.js
@@ -0,0 +1,19 @@
+﻿/* Event Handlers */
+
+
+$(document).ready(function () {
+    $("#sectionAdd").click(function () {
+        $("#genericSection").clone().appendTo("#addSectionButton");
+        //alert("Testing Alerts");
+    });
+});
+
+
+
+/* DOM Manipulation */
+
+function DeleteSection(SectionId) {
+    $(SectionId).Remove();
+}
+
+/* Data Access */
