ca65b95a22ace3d34067a9ff124e6f7db734cf15
Lacy Tesdall <templelacy@gmail.com>
2019-03-12 12:50:06 -0500

    Signed-off-by: Lacy Tesdall <templelacy@gmail.com>

--- BrainNotFound.Paper/BrainNotFound.Paper/Controllers/AdminController.cs
+++ BrainNotFound.Paper/BrainNotFound.Paper/Controllers/AdminController.cs
@@ -4,11 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using BrainNotFound.Paper.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using BrainNotFound.Paper.Models.BusinessModels;
-using BrainNotFound.Paper;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Model;
 using Microsoft.AspNetCore.Http;
 using BrainNotFound.Paper.Models.ViewModels;
@@ -25,7 +23,12 @@ namespace BrainNotFound.Paper.Controllers
         private readonly PaperDbContext _context;
 
         #region admin controllers
-        // Constructor
+        
+        /// <summary>
+        /// Constructor 
+        /// </summary>
+        /// <param name="userManager">Sets the UserManager</param>
+        /// <param name="context">Sets the database context</param>
         public AdminController(
             UserManager<ApplicationUser> userManager, PaperDbContext context)
         {
@@ -50,6 +53,9 @@ namespace BrainNotFound.Paper.Controllers
         #endregion admin controllers
 
         #region create administrator controllers
+        /// <summary>
+        /// Returns a list of all the administrators
+        /// </summary>
         [HttpGet, Route("Administrators")]
         public async Task<IActionResult> Administrators()
         {
@@ -58,12 +64,19 @@ namespace BrainNotFound.Paper.Controllers
             return View(allAdministrators);
         }
 
+        /// <summary>
+        /// GET: Displays the form for a new administrator
+        /// </summary>
         [HttpGet, Route("Administrators/New")]
         public IActionResult NewAdministrator()
         {
             return View();
         }
 
+        /// <summary>
+        /// POST: Creates a new administrator profile
+        /// </summary>
+        /// <param name="model">The admin info that is being added to the _userManager</param>
         [HttpPost, Route("Administrators/New")]
         public async Task<IActionResult> NewAdministrator(ApplicationUser model)
         {
@@ -108,15 +121,16 @@ namespace BrainNotFound.Paper.Controllers
             return View(model);
         }
 
+        /// <summary>
+        /// Views a specific administrator's information
+        /// </summary>
+        /// <param name="username">The specific admin to look up</param>
         [HttpGet, Route("Administrators/{UserName}")]
         public async Task<IActionResult> ViewAdministrator(String username)
         {
-
             var admin = await _userManager.FindByNameAsync(username);
 
-            ViewBag.profile = admin;
-
-            return View();
+            return View(admin);
         }
 
         [HttpGet, Route("Administrators/Edit/{UserName}")]
