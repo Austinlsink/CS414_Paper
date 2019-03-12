fc074535b472fa6116bead53f38fc62a9e2b5696
127321 <nick.goblirsch@students.pcci.edu>
2019-03-11 21:50:08 -0500

    Signed-off-by: 127321 <nick.goblirsch@students.pcci.edu>

---
+++ BrainNotFound.Paper/BrainNotFound.Paper/api/CreateTestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrainNotFound.Paper.api
+{
+    [Route("api")]
+    public class CreateTestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("GetSection")]
+        public string GetSection()
+        {
+            return "Nick is Awesome";
+        }       
+    }
+}
--- BrainNotFound.Paper/BrainNotFound.Paper/wwwroot/js/CreateTest.js
+++ BrainNotFound.Paper/BrainNotFound.Paper/wwwroot/js/CreateTest.js
@@ -1,19 +1,69 @@
 ﻿/* Event Handlers */
 
+//Add a generic section to the test
+var RootURL = "http://" + document.location.host;
 
 $(document).ready(function () {
     $("#sectionAdd").click(function () {
-        $("#genericSection").clone().appendTo("#contentBeforeButton");
+        $.ajax({
+            url: RootURL + "api/GetSection", success: function (result) {
+                $("#contentBeforeButton").append(result);
+            }
+        });
+        
+        //$("#contentBeforeButton").Append(GetSection("Generic"));
         //alert("Testing Alerts");
     });
 });
+
+//Change the generic section to a specified section
+$("#setQuestionType").click(function () {
+    if ("the selected option" == "True/False") {
+
+    }
+    else if ("the selected option" == "Multple Choice") {
+
+    }
+    else if ("the selected option" == "Matching") {
+
+    }
+    else if ("the selected option" == "Fill in the Blank") {
+
+    }
+    else if ("the selected option" == "Essay") {
+
+    }
+    
+}); 
 
 
 
 /* DOM Manipulation */
 
+//Delete a specific section from the test
 function DeleteSection(SectionId) {
     $(SectionId).Remove();
 }
 
+
 /* Data Access */
+
+function GetSection(QuestionType) {
+    $.ajax({
+        url: RootURL + "api/GetSection",
+        type: "GET",
+        data: { QuestionType: + "\"" + QuestionType + "\"" },
+        contentType: "application/json",
+        success: function (data) {
+            return data;
+        }
+    });
+}
+
+
+/* Template Functions */
+
+function HtmlForNewSection(SectionId) {
+
+
+}
