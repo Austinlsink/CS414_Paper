9e254b03800b99860998b6b649d78061cd71d0ee
127321 <nick.goblirsch@students.pcci.edu>
2019-03-12 15:50:50 -0500

    Signed-off-by: 127321 <nick.goblirsch@students.pcci.edu>

--- BrainNotFound.Paper/BrainNotFound.Paper/wwwroot/js/EditTest.js
+++ BrainNotFound.Paper/BrainNotFound.Paper/wwwroot/js/EditTest.js
@@ -1,62 +1,62 @@
-﻿/* Event Handlers */
+﻿///* Event Handlers */
 
-//Add a generic section to the test
-var RootURL = "http://" + document.location.host + "/";
+////Add a generic section to the test
+//var RootURL = "http://" + document.location.host + "/";
 
-$("#EditNameAndCourseBT").click(function () {
+////$("#EditNameAndCourseBT").click(function () {
 
-    $.ajax({
-        url: RootURL + "Instructor/Tests/Partials/EditNameAndCourse/132",
-        success: function (result) {
-            $("#x_c-infoSection").prepend(result);
+////    $.ajax({
+////        url: RootURL + "Instructor/Tests/Partials/EditNameAndCourse/131",
+////        success: function (result) {
+////            $("#x_c-infoSection").prepend(result);
 
-        }
-    });
-});
+////        }
+////    });
+////});
 
-//Change the generic section to a specified section
-//$("#setQuestionType").click(function () {
-//    if ("the selected option" == "True/False") {
+////Change the generic section to a specified section
+////$("#setQuestionType").click(function () {
+////    if ("the selected option" == "True/False") {
 
-//    }
-//    else if ("the selected option" == "Multple Choice") {
+////    }
+////    else if ("the selected option" == "Multple Choice") {
 
-//    }
-//    else if ("the selected option" == "Matching") {
+////    }
+////    else if ("the selected option" == "Matching") {
 
-//    }
-//    else if ("the selected option" == "Fill in the Blank") {
+////    }
+////    else if ("the selected option" == "Fill in the Blank") {
 
-//    }
-//    else if ("the selected option" == "Essay") {
+////    }
+////    else if ("the selected option" == "Essay") {
 
-//    }
+////    }
     
-//    })
+////    })
 
 
 
 
-///* DOM Manipulation */
+/////* DOM Manipulation */
 
-////Delete a specific section from the test
-//function DeleteSection(SectionId) {
-//    $(SectionId).Remove();
-//}
+//////Delete a specific section from the test
+////function DeleteSection(SectionId) {
+////    $(SectionId).Remove();
+////}
 
 
-///* Data Access */
+/////* Data Access */
 
-//function GetSection(QuestionType) {
+////function GetSection(QuestionType) {
     
-//}
+////}
 
 
-///* Template Functions */
+/////* Template Functions */
 
-//function toggle(source, elementName) {
-//    checkboxes = document.getElementsByName(elementName);
-//    for (var i = 0, n = checkboxes.length; i < n; i++) {
-//        checkboxes[i].checked = source.checked;
-//    }
-//}
+////function toggle(source, elementName) {
+////    checkboxes = document.getElementsByName(elementName);
+////    for (var i = 0, n = checkboxes.length; i < n; i++) {
+////        checkboxes[i].checked = source.checked;
+////    }
+////}
