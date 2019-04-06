d7ce65d6be85f733c676c1612171cf51f3e5129d
Lacy Tesdall <templelacy@gmail.com>
2019-04-06 12:14:19 -0500

    Signed-off-by: Lacy Tesdall <templelacy@gmail.com>

--- BrainNotFound.Paper/BrainNotFound.Paper/wwwroot/js/TakeTest.js
+++ BrainNotFound.Paper/BrainNotFound.Paper/wwwroot/js/TakeTest.js
@@ -1,46 +1,38 @@
 ﻿// Submits the test
-$("button#submit").click(function () {
-    var newInstructorForm = $("form#NewInstructorForm");
-
-    // Gets the values of the form, and creates an object to be sent to the server
-    var instructor = {};
-    $.each(newInstructorForm.serializeArray(), function (i, field) {
-        instructor[field.name] = field.value;
-    });
-
-    // Creates, submits, and responds to Ajax Call
+$(".submitTest").click(function () {
+    var TestScheduleId = $("input#testScheduleId").val();
+    console.log("In the submit test function");
     $.ajax({
-        url: "/api/Instructor/New/",
+        url: "/api/Tests/SubmitTest/",
         type: "POST",
         contentType: "application/json",
         // Data fetched from the form
-        data: JSON.stringify(instructor),
+        data: TestScheduleId,
         success: function (result) {
             // Close the modal window
             console.log(result.message);
-            location.reload();
+            location.replace("Student/Grades");
         },
         error: function (xhr, status, error) {
-            console.log(xhr.responseText);
-            var err = JSON.parse(xhr.responseText);
+            console.log(error);
+            
         }
     })
 })
-
-
-
 
 // This function confirms that when the student submits the test, the pledge and his name match and that all of the questions are answered
 $("button#submitTest").click(function () {
     var nameInput = document.getElementById("fullnameInput").value;
     var studentName = $("span#fullname").attr("data-studentName");
 
+    // Error check the student signature
     if (nameInput === "") {
         $("h4#myModalLabel2").text("Warning:");
         $("p#errorMessagePlaceHolder").text("Submitting the test without a pledge will result in a 0. Are you sure you want to proceed?");
         $("#ErrorModal").modal("toggle");
     }
     else if (nameInput.trim() === studentName.trim()) {
+        $("#pledgeErrorMessage").addClass("hidden");
         // Now verify that all of the questions have been answered
         var TestScheduleId = $("input#testScheduleId").val();
         $.ajax({
@@ -50,26 +42,20 @@ $("button#submitTest").click(function () {
             // Data fetched from the form
             data: TestScheduleId,
             success: function (result) {
-                $("h4#myModalLabel2").text("Success!");
-                $("p#errorMessagePlaceHolder").text("You've finished the test. You may now review the results.");
+                $("p#successModalPlaceholder").text("You've finished the test. You may now review the results.");
                 $("#SuccessModal").modal("toggle");
             },
             error: function (xhr, status, error) {
-                console.log(xhr.responseText);
-                var err = JSON.parse(xhr.responseText);
+                $("p#questionModalPlaceholder").text("All questions have not been answered. Are you sure you want to submit the test?");
+                $("#QuestionsAnsweredModal").modal("toggle");
             }
         })
-
-        
     }
     else {
-        $("h4#myModalLabel2").text("Error!");
-        $("p#errorMessagePlaceHolder").text("The pledge does not match. Please re-enter your name.");
-        document.getElementById("MessageNo").style.display = "none";
-        document.getElementById("MessageYes").innerText = "Ok";
-        $("#ErrorModal").modal("toggle");
+        $("#pledgeErrorMessage").removeClass("hidden");
     }
 })
+
 
 // This function reponds to the radios on change event - we're grabbing data!!!!!
 $("input[type='radio']").on("change", function () {
@@ -109,9 +95,7 @@ $(".multipleChoiceOption").click(function () {
     else {
         $(this).removeClass("btn-primary").addClass("btn-default");
     }
-
 })
-
 
 // stripes every other row
 $(document).ready(function () {
