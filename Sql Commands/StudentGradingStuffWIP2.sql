8c27a52a38edd2aa395af22e90c6b762543a28bc
Kara Funda <129675@studentnet.int>
2019-04-12 17:52:52 -0500

    Signed-off-by: Kara Funda <129675@studentnet.int>

--- Sql Commands/StudentGradingStuffWIP2.sql
+++ Sql Commands/StudentGradingStuffWIP2.sql
@@ -1,6 +1,6 @@
 USE [CS414_BrainNotFound]
 GO
-/****** Object:  Trigger [dbo].[gradeTest]    Script Date: 4/10/2019 9:27:50 PM ******/
+/****** Object:  Trigger [dbo].[gradeTest]    Script Date: 4/12/2019 4:53:01 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -10,45 +10,39 @@ AFTER UPDATE
 AS
 IF (UPDATE (Submitted)) --If the submitted column has changed
 BEGIN
-	-- Get all tests submitted at time of update
+	-- Get the ids of all tests submitted at time of update
 	DECLARE studentTestAssignmentCursor CURSOR FOR 
-		SELECT StudentTestAssignmentId, StudentId, Submitted, Grade, Signed, TestScheduleId
-		  FROM inserted;
-	DECLARE @studentTestAssignmentId BIGINT,
-			@studentId BIGINT,
-			@submitted BIT,
-			@grade REAL,
-			@signed BIT,
-			@testScheduleId BIGINT;
+		SELECT StudentTestAssignments.StudentTestAssignmentId
+		  FROM StudentTestAssignments JOIN inserted ON StudentTestAssignments.StudentTestAssignmentId = inserted.StudentTestAssignmentId;
+	DECLARE @studentTestAssignmentId BIGINT;
 
-	OPEN studentTestAssignmentCursor;
+	OPEN studentTestAssignmentCursor
+	FETCH NEXT FROM studentTestAssignmentCursor INTO @studentTestAssignmentId;
 
-	FETCH NEXT FROM studentTestAssignmentCursor INTO @studentTestAssignmentId, @studentId, @submitted, @grade, @signed, @testScheduleId;
-
-	-- Go through each test
 	WHILE @@FETCH_STATUS = 0
 	BEGIN
 		-- If the test has been submitted
-		IF (@submitted = 1)
+		IF ((SELECT Submitted
+			   FROM StudentTestAssignments
+			  WHERE StudentTestAssignmentId = @studentTestAssignmentId) = 1)
 		BEGIN
 			-- If the pledge has been signed, grade the test
-			IF (@signed = 1)
+			IF ((SELECT Signed
+				   FROM StudentTestAssignments
+				  WHERE StudentTestAssignmentId = @studentTestAssignmentId) = 1)
 			BEGIN
-
 				-- The overall grade the student has
 				DECLARE @c_testGrade int = 0;
 				-- Store the AnswerId you get from the cursor below
 				DECLARE @c_AnswerId bigint;
 				-- Create a cursor for all the answers a student put on this test
 				DECLARE studentTestCursor CURSOR FOR
-					SELECT StudentAnswers.AnswerId --, Questions.QuestionId
-					  FROM StudentTestAssignments JOIN AspNetUsers ON StudentTestAssignments.StudentId = AspNetUsers.Id
-												  JOIN StudentAnswers ON AspNetUsers.Id = StudentAnswers.StudentId
-												  JOIN TestSchedules ON StudentAnswers.TestScheduleId = TestSchedules.TestScheduleId
+					SELECT StudentAnswers.AnswerId
+					  FROM StudentTestAssignments JOIN TestSchedules ON StudentTestAssignments.TestScheduleId = TestSchedules.TestScheduleId
+												  JOIN StudentAnswers ON TestSchedules.TestScheduleId = StudentAnswers.TestScheduleId
 					 WHERE StudentTestAssignments.StudentTestAssignmentId = @studentTestAssignmentId;
 					
 				OPEN studentTestCursor;
-
 				FETCH NEXT FROM studentTestCursor INTO @c_AnswerId;
 
 				-- Loop through all student answers
@@ -62,7 +56,10 @@ BEGIN
 					-- Grade Essay answers (tell teacher that manual grading is required)
 					IF ((SELECT Questions.QuestionType
 						   FROM Questions JOIN StudentAnswers ON Questions.QuestionId = StudentAnswers.QuestionId
-						  WHERE StudentAnswers.AnswerId = @c_AnswerId) = 'Essay')
+										  JOIN TestSchedules ON StudentAnswers.TestScheduleId = TestSchedules.TestScheduleId
+										  JOIN StudentTestAssignments ON TestSchedules.TestScheduleId = StudentTestAssignments.TestScheduleId
+							   WHERE StudentAnswers.AnswerId = @c_AnswerId
+								 AND StudentTestAssignments.StudentTestAssignmentId = @studentTestAssignmentId) = 'Essay')
 					BEGIN
 						UPDATE StudentTestAssignments
 							SET ManualGradingRequired = 1
@@ -73,7 +70,10 @@ BEGIN
 					-- Grade FillInTheBlank answers
 					ELSE IF ((SELECT Questions.QuestionType
 								FROM Questions JOIN StudentAnswers ON Questions.QuestionId = StudentAnswers.QuestionId
-							   WHERE StudentAnswers.AnswerId = @c_AnswerId) = 'FillInTheBlank')
+											   JOIN TestSchedules ON StudentAnswers.TestScheduleId = TestSchedules.TestScheduleId
+											   JOIN StudentTestAssignments ON TestSchedules.TestScheduleId = StudentTestAssignments.TestScheduleId
+							   WHERE StudentAnswers.AnswerId = @c_AnswerId
+								 AND StudentTestAssignments.StudentTestAssignmentId = @studentTestAssignmentId) = 'FillInTheBlank')
 					BEGIN
 						SELECT @correct = dbo.gradeFillInTheBlank(@c_AnswerId);
 					END; -- End grading a FillInTheBlank question
@@ -81,7 +81,10 @@ BEGIN
 					-- Grade Matching answers
 					ELSE IF ((SELECT Questions.QuestionType
 								FROM Questions JOIN StudentAnswers ON Questions.QuestionId = StudentAnswers.QuestionId
-							   WHERE StudentAnswers.AnswerId = @c_AnswerId) = 'Matching')
+											   JOIN TestSchedules ON StudentAnswers.TestScheduleId = TestSchedules.TestScheduleId
+											   JOIN StudentTestAssignments ON TestSchedules.TestScheduleId = StudentTestAssignments.TestScheduleId
+							   WHERE StudentAnswers.AnswerId = @c_AnswerId
+								 AND StudentTestAssignments.StudentTestAssignmentId = @studentTestAssignmentId) = 'Matching')
 					BEGIN
 						SELECT @correct = dbo.gradeMatching(@c_AnswerId);
 					END; -- End grading a Matching question
@@ -89,7 +92,10 @@ BEGIN
 					-- Grade MultipleChoice answers
 					ELSE IF ((SELECT Questions.QuestionType
 							    FROM Questions JOIN StudentAnswers ON Questions.QuestionId = StudentAnswers.QuestionId
-							   WHERE StudentAnswers.AnswerId = @c_AnswerId) = 'MultipleChoice')
+											   JOIN TestSchedules ON StudentAnswers.TestScheduleId = TestSchedules.TestScheduleId
+											   JOIN StudentTestAssignments ON TestSchedules.TestScheduleId = StudentTestAssignments.TestScheduleId
+							   WHERE StudentAnswers.AnswerId = @c_AnswerId
+								 AND StudentTestAssignments.StudentTestAssignmentId = @studentTestAssignmentId) = 'MultipleChoice')
 					BEGIN
 						SELECT @correct = dbo.gradeMultipleChoice(@c_AnswerId);
 					END; -- End grading a MultipleChoice question
@@ -97,7 +103,10 @@ BEGIN
 					-- Grade TrueFalse answers
 					ELSE IF ((SELECT Questions.QuestionType
 					            FROM Questions JOIN StudentAnswers ON Questions.QuestionId = StudentAnswers.QuestionId
-							   WHERE StudentAnswers.AnswerId = @c_AnswerId) = 'TrueFalse')
+											   JOIN TestSchedules ON StudentAnswers.TestScheduleId = TestSchedules.TestScheduleId
+											   JOIN StudentTestAssignments ON TestSchedules.TestScheduleId = StudentTestAssignments.TestScheduleId
+							   WHERE StudentAnswers.AnswerId = @c_AnswerId
+								 AND StudentTestAssignments.StudentTestAssignmentId = @studentTestAssignmentId) = 'TrueFalse')
 					BEGIN
 						SELECT @correct = dbo.gradeTrueFalse(@c_AnswerId);
 					END; -- End grading a TrueFalse question
@@ -106,7 +115,9 @@ BEGIN
 
 					-- Add points to the test if the answer was correct
 					IF (@correct = 1)
-						SELECT @c_testGrade += (SELECT Questions.PointValue FROM Questions JOIN StudentAnswers ON Questions.QuestionId = StudentAnswers.QuestionId WHERE StudentAnswers.AnswerId = @c_AnswerId);
+						SELECT @c_testGrade += (SELECT Questions.PointValue
+												  FROM Questions JOIN StudentAnswers ON Questions.QuestionId = StudentAnswers.QuestionId
+											  	 WHERE StudentAnswers.AnswerId = @c_AnswerId);
 
 					-- Get next answer to grade
 					FETCH NEXT FROM studentTestCursor INTO @c_AnswerId;
@@ -117,23 +128,23 @@ BEGIN
 
 				-- Set the grade for the test
 				UPDATE StudentTestAssignments
-					SET Grade = @c_testGrade
-					WHERE StudentTestAssignments.StudentTestAssignmentId = @studentTestAssignmentId;
-			END; -- End grading when student has signed the pledge
+				   SET Grade = @c_testGrade
+				 WHERE StudentTestAssignments.StudentTestAssignmentId = @studentTestAssignmentId;
+			END; -- End grading when the student has signed the pledge
 
 			-- If the pledge has not been signed, set the grade to 0
 			ELSE
 			BEGIN
 				UPDATE StudentTestAssignments
 					SET Grade = 0
-					WHERE StudentTestAssignments.StudentTestAssignmentId = @studentTestAssignmentId;
-			END; -- End grading when student has not signed the pledge
-		END; -- End check for pledge
-
+				  WHERE StudentTestAssignments.StudentTestAssignmentId = @studentTestAssignmentId;
+			END; -- End check for pledge
+		END; -- End check for if the test was submitted
+		
 		-- Get the next test to grade
-		FETCH NEXT FROM studentTestAssignmentCursor INTO @studentTestAssignmentId, @studentId, @submitted, @grade, @signed, @testScheduleId;
+		FETCH NEXT FROM studentTestAssignmentCursor INTO @studentTestAssignmentId;
 	END; -- End going through each test
 
 	CLOSE studentTestAssignmentCursor;
 	DEALLOCATE studentTestAssignmentCursor;
-END;
+END; -- End going through each test
