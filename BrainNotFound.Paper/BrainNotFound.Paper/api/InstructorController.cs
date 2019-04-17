06633dfdef695c0a53dd5c9c4ed4db9d14237c5b
Lacy Tesdall <templelacy@gmail.com>
2019-04-17 09:21:52 -0500

    Doccumented, cleaned code, fixed spelling for api instructorController
    Signed-off-by: Lacy Tesdall <templelacy@gmail.com>

--- BrainNotFound.Paper/BrainNotFound.Paper/api/InstructorController.cs
+++ BrainNotFound.Paper/BrainNotFound.Paper/api/InstructorController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using BrainNotFound.Paper.Models.BusinessModels;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,16 +12,16 @@ namespace BrainNotFound.Paper.api
     [ApiController]
     public class InstructorController : Controller
     {
-
         #region Initialize controllers
+
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly PaperDbContext _context;
+
         /// <summary>
         /// Constructor 
         /// </summary>
         /// <param name="userManager">Sets the UserManager</param>
         /// <param name="context">Sets the database context</param>
-        /// 
         public InstructorController(UserManager<ApplicationUser> userManager, PaperDbContext context)
         {
             _userManager = userManager;
@@ -33,10 +30,14 @@ namespace BrainNotFound.Paper.api
 
         #endregion Initialize Controllers
 
+        /// <summary>
+        /// Allows the admin to delete an instructor as long as the instructor is note assigned to any sections
+        /// </summary>
+        /// <param name="instructorId">Search criteria for finding the specific instructor</param>
+        /// <returns>Json result of either true if deleting the instructor is successful; otherwise, false</returns>
         [HttpPost, Route("Delete")]
         public async Task<IActionResult> Delete([FromBody]string instructorId)
         {
-            
             var instructor = await _userManager.FindByIdAsync(instructorId);
 
             if (_context.Sections.Where(s => s.InstructorId == instructor.Id).Any())
@@ -51,10 +52,10 @@ namespace BrainNotFound.Paper.api
         }
 
         /// <summary>
-        /// Allows you to edit an Instructor
+        /// Allows the admin to edit an instructor's information
         /// </summary>
-        /// <param name="username"></param>
-        /// <returns></returns>
+        /// <param name="username">Search criteria for finding the specific instructor</param>
+        /// <returns>Json result of true and the instructor's profile information</returns>
         [HttpPost, Route("Edit/{username}")]
         public async Task<IActionResult> Edit([FromBody]string username)
         {
@@ -72,6 +73,12 @@ namespace BrainNotFound.Paper.api
                                              dob = instructor.DOB});
         }
 
+        /// <summary>
+        /// Allows the admin to save the edited changes to an instructor
+        /// </summary>
+        /// <param name="user">ApplicationUser object that contains the new information</param>
+        /// <param name="username">Saerch criteria for the specific instructor</param>
+        /// <returns>Json result of either true if an instructor was successfully updated; otherwise, false</returns>
         [HttpPost, Route("SaveChanges")]
         public async Task<IActionResult> SaveChanges([FromBody] ApplicationUser user, string username)
         {
@@ -101,10 +108,10 @@ namespace BrainNotFound.Paper.api
         }
 
         /// <summary>
-        /// Create a new Instructor
+        /// Allows the admin to create a new instructor
         /// </summary>
-        /// <param name="user">Instructor information</param>
-        /// <returns></returns>
+        /// <param name="user">ApplicationUser object that contains all of the needed information</param>
+        /// <returns>Json result of either true if the instructor is successfully created; otherwise, false</returns>
         [HttpPost, Route("New")]
         public async Task<IActionResult> New([FromBody] ApplicationUser user)
         {
