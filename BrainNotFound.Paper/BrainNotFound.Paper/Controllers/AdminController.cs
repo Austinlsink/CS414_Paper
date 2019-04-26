70a9ba79bab8a1e1443f69e6f9faa5cb27555533
Lacy Tesdall <templelacy@gmail.com>
2019-04-25 19:08:43 -0500

    Signed-off-by: Lacy Tesdall <templelacy@gmail.com>

--- BrainNotFound.Paper/BrainNotFound.Paper/Controllers/AdminController.cs
+++ BrainNotFound.Paper/BrainNotFound.Paper/Controllers/AdminController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Http;
 using System.Data.SqlClient;
 using System.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace BrainNotFound.Paper.Controllers
 {
@@ -664,17 +665,17 @@ namespace BrainNotFound.Paper.Controllers
             ViewBag.students = students;
 
             // Find all enrollments and add them to the ViewBag
-            var enrollment = _context.Enrollments.Include(x => x.ApplicationUser).Include(x => x.Section);
+            var enrollment = _context.Enrollments.Include(x => x.ApplicationUser).Include(x => x.Section).Where(x => x.SectionId == section.SectionId).Select(x => x.ApplicationUser);
             ViewBag.enrollment = enrollment;
 
-
             //Find all students not enrolled in the course
-            var studentsNotEnrolled = _context.Enrollments.Include(x => x.ApplicationUser).Where(x => x.SectionId != section.SectionId).Select(x => x.ApplicationUser).Distinct().ToList();
+            var studentsNotEnrolled = _context.Enrollments.Include(x => x.ApplicationUser).Include(x => x.Section).Where(x => x.SectionId != section.SectionId).Select(x => x.ApplicationUser).Except(enrollment).Distinct();
 
             // Find all the SectionMeetingTimes and add them to the ViewBag
             var sectionMeetingTimeList = _context.SectionMeetingTimes.ToList();
             ViewBag.sectionMeetingTimeList = sectionMeetingTimeList;
             ViewBag.StudentsNotEnrolled = studentsNotEnrolled;
+
 
             return View();
         }
