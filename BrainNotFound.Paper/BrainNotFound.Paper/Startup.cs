b02316c199ec21968595838e475041d3c6337a79
Abmael <abmael.silva@me.com>
2019-04-25 21:58:26 -0500

    Signed-off-by: Abmael <abmael.silva@me.com>

--- BrainNotFound.Paper/BrainNotFound.Paper/Startup.cs
+++ BrainNotFound.Paper/BrainNotFound.Paper/Startup.cs
@@ -67,16 +67,15 @@ namespace BrainNotFound.Paper
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-            //else
-            //{
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
 
-            //    app.UseStatusCodePagesWithReExecute("/Error", "?statusCode={0}");
-            //}
-            app.UseStatusCodePagesWithReExecute("/Error", "?statusCode={0}");
+                app.UseStatusCodePagesWithReExecute("/Error", "?statusCode={0}");
+            }
             app.UseStaticFiles();
 
             app.UseAuthentication();
